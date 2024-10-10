d7c67ceb266a037f79dfd0297f95653966796276
Rapphy243 <36800640+rapphy243@users.noreply.github.com>
2024-10-10 09:38:48 -0500

    Step 2

--- Tic Tac Toe/ContentView.swift
+++ Tic Tac Toe/ContentView.swift
@@ -10,12 +10,22 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Text("Tic Tac Toe")
+            LazyVGrid(columns: Array(repeating: GridItem(.fixed(120)), count: 3), content: {
+                ForEach(0..<9) { index in
+                    ZStack {
+                        Color.blue
+                        Text("X")
+                            .font(.system(size: 90))
+                            .fontWeight(.heavy)
+                    }
+                    .frame(width: 120, height: 120, alignment: .center)
+                    .cornerRadius(30)
+                    
+                }
+            })
         }
-        .padding()
+        .preferredColorScheme(.dark)
     }
 }
 
