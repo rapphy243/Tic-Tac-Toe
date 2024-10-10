7394d4ec7201f755418592488f12d0e20220b9a7
Rapphy243 <36800640+rapphy243@users.noreply.github.com>
2024-10-10 10:02:33 -0500

    Step 5

--- Tic Tac Toe/ContentView.swift
+++ Tic Tac Toe/ContentView.swift
@@ -8,6 +8,8 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var moves =  Array(repeating: "", count: 9)
+    @State private var isXturn = true
     var body: some View {
         VStack {
             Text("Tic Tac Toe")
@@ -15,13 +17,20 @@ struct ContentView: View {
                 ForEach(0..<9) { index in
                     ZStack {
                         Color.blue
-                        Text("X")
+                        Text(moves[index])
                             .font(.system(size: 90))
                             .fontWeight(.heavy)
                     }
                     .frame(width: 120, height: 120, alignment: .center)
                     .cornerRadius(30)
-                    
+                    .onTapGesture {
+                        withAnimation(.default){
+                            if moves[index] == "" {
+                                moves[index] = isXturn ? "X" : "O"
+                                isXturn.toggle()
+                            }
+                        }
+                    }
                 }
             })
         }
