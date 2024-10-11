f820f90d928fb6a0d8dd38c764b4eccb14dd9bb4
Rapphy243 <36800640+rapphy243@users.noreply.github.com>
2024-10-11 09:42:49 -0500

    Finish Project

--- Tic Tac Toe/ContentView.swift
+++ Tic Tac Toe/ContentView.swift
@@ -10,6 +10,8 @@ import SwiftUI
 struct ContentView: View {
     @State private var moves =  Array(repeating: "", count: 9)
     @State private var isXturn = true
+    @State private var gameOver = false
+    @State private var winMessage = ""
     var body: some View {
         VStack {
             Text("Tic Tac Toe")
@@ -17,6 +19,8 @@ struct ContentView: View {
                 ForEach(0..<9) { index in
                     ZStack {
                         Color.blue
+                        Color.white
+                            .opacity(moves[index] == "" ? 1 : 0)
                         Text(moves[index])
                             .font(.system(size: 90))
                             .fontWeight(.heavy)
@@ -31,10 +35,45 @@ struct ContentView: View {
                             }
                         }
                     }
+                    .rotation3DEffect(
+                        .degrees(moves[index] == "" ? 180 : 0), axis: (x: 0.0, y: 1.0, z: 0.0))
                 }
             })
         }
         .preferredColorScheme(.dark)
+        .alert(isPresented: $gameOver) {
+            Alert(title: Text(winMessage), dismissButton: .destructive(Text("Play Again?"),                      action : {
+                withAnimation {
+                    moves = Array(repeating: "", count: 9)
+                    gameOver = false
+                }
+            }))
+        }
+        .onChange(of: moves) { oldValue, newValue in
+            checkForWinner()
+            if !(gameOver || moves.contains("")) {
+                winMessage = "Cat's Game!"
+                gameOver = true
+            }
+        }
+        
+    }
+    private func checkForWinner() {
+        checkLine(a: 0, b: 1, c: 2) //top row
+        checkLine(a: 3, b: 4, c: 5) //middle row
+        checkLine(a: 6, b: 7, c: 8) //bottom row
+        checkLine(a: 0, b: 3, c: 6) //first column
+        checkLine(a: 1, b: 4, c: 7) //second column
+        checkLine(a: 2, b: 5, c: 8) //third column
+        checkLine(a: 0, b: 4, c: 8) // \ diagonal
+        checkLine(a: 2, b: 4, c: 6) // / diagonal
+    }
+    
+    private func checkLine(a: Int, b: Int, c: Int) {
+        if moves[a] != "" && moves[a] == moves[b] && moves[b] == moves[c] {
+            winMessage = "\(moves[a]) is the winner!"
+            gameOver = true
+        }
     }
 }
 
